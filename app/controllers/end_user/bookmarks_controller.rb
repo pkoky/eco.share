75fb722bc30c701a245263b6aaa729b09c220334
pkoky <sscsk2224@gmail.com>
2021-05-26 08:35:56 +0000

    add

--- app/controllers/end_user/bookmarks_controller.rb
+++ app/controllers/end_user/bookmarks_controller.rb
@@ -1,23 +1,23 @@
 class EndUser::BookmarksController < ApplicationController
-  
-  
+
+
   def bookmarks
-    @bookmarks = Bookmark.where(end_user_id: current_end_user.id)
-    
+    @bookmarks = Bookmark.where(end_user_id: current_end_user.id).page(params[:page]).per(9)
+
   end
-  
+
   def create
     @postProduct = PostProduct.find(params[:post_product_id])
     bookmark = @postProduct.bookmarks.new(end_user_id: current_end_user.id)
     if bookmark.save
     end
-  end 
-  
+  end
+
   def destroy
     @postProduct = PostProduct.find(params[:post_product_id])
     bookmark = @postProduct.bookmarks.find_by(end_user_id: current_end_user.id)
     if bookmark.present?
       bookmark.destroy
-    end 
-  end 
+    end
+  end
 end
