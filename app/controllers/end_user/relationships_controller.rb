dd3b31a698ddf2007e4e7542268fbd6f0ef2d4a0
pkoky <sscsk2224@gmail.com>
2021-05-26 07:49:29 +0000

    フォロワー一覧作成

--- app/controllers/end_user/relationships_controller.rb
+++ app/controllers/end_user/relationships_controller.rb
@@ -9,6 +9,15 @@ class EndUser::RelationshipsController < ApplicationController
     current_end_user.unfollow(params[:id])
   end
 
+  def follower
+    end_user = EndUser.find(params[:id])
+    @follower = end_user.follower_end_user
+  end
+
+  def followed
+    end_user = EndUser.find(params[:id])
+    @followed = end_user.following_end_user
+  end
 
 
 
